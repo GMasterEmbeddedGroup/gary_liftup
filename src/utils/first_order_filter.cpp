0298a1914a5b9033d996cb39e26a79eea1231193
Williamyxn <williamyxn@gmail.com>
2023-07-30 18:05:22 +0800

    update: slow stop

--- src/utils/first_order_filter.cpp
+++ src/utils/first_order_filter.cpp
@@ -16,7 +16,7 @@ double First_orderFilter::first_order_filter(double input) {
     double delta_accel = this->max_accel * delta_time;
     double delta_input = input - this->out;
     if (delta_input > delta_accel) delta_input = delta_accel;
-    if (delta_input < - delta_accel) delta_input = - delta_accel;
+    if (delta_input < 0.0 - (2.0*delta_accel)) delta_input = 0.0 - (2.0*delta_accel);
     this->out += delta_input;
     if(this->out >= 1.9) this->out = 1.9;
     if(this->out <= -1.9) this->out = -1.9;
@@ -24,6 +24,6 @@ double First_orderFilter::first_order_filter(double input) {
 }
 
 void First_orderFilter::reset() {
-    this->out = 0;
-    this->last_time = rclcpp::Clock{RCL_SYSTEM_TIME}.now().seconds();
+//    this->out = 0;
+//    this->last_time = rclcpp::Clock{RCL_SYSTEM_TIME}.now().seconds();
 }
